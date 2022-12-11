3be0cfd2dab7c699d8f3cff01ce0f667cbbfebc8
mongsil1025 <sunminjsm@gmail.com>
2022-12-12 07:03:27 +0900

    Add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,34 @@
-import { Box } from "@mui/material";
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Button,
+  IconButton,
+  Stack,
+} from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 interface HeaderProps {
   link: string;
 }
 
 export const Header = (props: HeaderProps) => {
+  function copy() {
+    navigator.clipboard.writeText(props.link);
+  }
+
   return (
     <>
-      {/* <Box variation="success" heading={`Hello👋 주소를 복사해두세요!`}>
-        {props.link}
-      </Box> */}
+      <Alert severity="success">
+        <AlertTitle>Hello👋 주소를 복사해두세요!</AlertTitle>
+
+        <Stack direction="row" alignItems="center">
+          <span>{props.link}</span>
+          <IconButton aria-label="copy" size="small" onClick={copy}>
+            <ContentCopyIcon fontSize="small" />
+          </IconButton>
+        </Stack>
+      </Alert>
     </>
   );
 };
